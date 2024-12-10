5fdfbe796ac3663b2aa412fd91eb5cadfc0670af
core.pager=cat <core.pager=cat@gmail.com>
2024-12-10 11:05:53 +0100

    Benefits Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
-import Button from "./components/Button";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
-        <Button />
+        <Benefits />
       </div>
       <ButtonGradient />
     </>
